661e9fb3c9d23f3b5dc0f22e50d4ecdd5e6bc686
Sumit Khandelwal <synergy2411@outlook.com>
2023-04-03 12:51:22 +0530

    new expense added

--- first-app/src/Components/Expenses/AddExpense/AddExpense.js
+++ first-app/src/Components/Expenses/AddExpense/AddExpense.js
@@ -1,30 +1,58 @@
-const AddExpense = () => {
+import { useRef, useState } from "react";
+import { v4 } from 'uuid';
+
+const AddExpense = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+
+    const createdAtRefInput = useRef()
+
+    const titleChangeHandler = event => setEnteredTitle(event.target.value)
+
+    const submitHandler = event => {
+        event.preventDefault();
+        console.log(enteredTitle, enteredAmount, createdAtRefInput.current.value);
+        let newExpense = {
+            id: v4(),
+            title: enteredTitle,
+            amount: Number(enteredAmount),
+            createdAt: new Date(createdAtRefInput.current.value)
+        }
+        props.onAddNewExpense(newExpense)
+    }
+
     return (
         <div className="row">
             <div className="col-6 offset-3">
                 <div className="card">
                     <div className="card-body">
                         <h4 className="text-center">Add Expenses</h4>
-                        <form>
+                        <form onSubmit={submitHandler}>
                             {/* title */}
                             <div className="form-group mb-3">
                                 <label>Title:</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control"
+                                    value={enteredTitle}
+                                    onChange={titleChangeHandler} />
                             </div>
                             {/* amount */}
                             <div className="form-group mb-3">
                                 <label>Amount:</label>
-                                <input type="number" min="0.1" step="0.1" className="form-control" />
+                                <input type="number" min="0.1" step="0.1" className="form-control"
+                                    value={enteredAmount}
+                                    onChange={event => setEnteredAmount(event.target.value)} />
                             </div>
                             {/* createdAt */}
                             <div className="form-group mb-3">
                                 <label>Date:</label>
-                                <input type="date" className="form-control" min="2022-04-01" max="2023-03-31" />
+                                <input type="date" className="form-control"
+                                    min="2022-04-01" max="2023-03-31"
+                                    ref={createdAtRefInput} />
                             </div>
                             {/* button */}
                             <div className="form-group">
                                 <div className="d-grid">
-                                    <button className="btn btn-success">Add</button>
+                                    <button className="btn btn-success" type="submit">Add</button>
                                 </div>
                             </div>
                         </form>
--- first-app/src/Components/Expenses/Expenses.js
+++ first-app/src/Components/Expenses/Expenses.js
@@ -3,25 +3,27 @@ import ExpenseItem from "./ExpenseItem/ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter/ExpenseFilter";
 import AddExpense from "./AddExpense/AddExpense";
 
+const INTIAL_EXPENSES = [{
+    id: "e001",
+    title: "shopping",
+    amount: 12.99,
+    createdAt: new Date("Dec 21, 2022")
+}, {
+    id: "e002",
+    title: "planting",
+    amount: 10.9,
+    createdAt: new Date("Jan 1, 2023")
+}, {
+    id: "e003",
+    title: "grocery",
+    amount: 29.9,
+    createdAt: new Date("Mar 20, 2023")
+}]
+
 const Expenses = () => {
 
-    let expenses = [{
-        id: "e001",
-        title: "shopping",
-        amount: 12.99,
-        createdAt: new Date("Dec 21, 2022")
-    }, {
-        id: "e002",
-        title: "planting",
-        amount: 10.9,
-        createdAt: new Date("Jan 1, 2023")
-    }, {
-        id: "e003",
-        title: "grocery",
-        amount: 29.9,
-        createdAt: new Date("Mar 20, 2023")
-    }]
 
+    const [expenses, setExpenses] = useState(INTIAL_EXPENSES)
     const [show, setShow] = useState(false)
     const [selectedYear, setSelectedYear] = useState('2023')
 
@@ -29,6 +31,11 @@ const Expenses = () => {
 
     const onYearSelect = (selYear) => {
         setSelectedYear(selYear)
+    }
+
+    const onAddNewExpense = (newExpense) => {
+        // Use function in SetState method when next state is depend upon previous state
+        setExpenses((prevState) => [newExpense, ...prevState])
     }
 
     const filteredExpenses = expenses.filter(exp =>
@@ -51,10 +58,11 @@ const Expenses = () => {
                         selectedYear={selectedYear} />
                 </div>
             </div>
-            {show && <AddExpense />}
+
+            {show && <AddExpense onAddNewExpense={onAddNewExpense} />}
+
             <div className="row">
                 {filteredExpenses.map(exp => <ExpenseItem key={exp.id} expense={exp} />)}
-
             </div>
         </div>
     )
