2633771ccc0194f69fed5252f4faf2431f24f06c
Sumit Khandelwal <synergy2411@outlook.com>
2023-04-04 12:59:01 +0530

    todo slice added

--- 04-redux-basics/app.js
+++ 04-redux-basics/app.js
@@ -3,41 +3,135 @@
 const { configureStore, createSlice } = require("@reduxjs/toolkit");
 
 const initialState = {
-    counter: 0
+    todos: []
 }
 
-const counterSlice = createSlice({
-    name: "counter",
-    initialState,
+const todoSlice = createSlice({
+    name: "todos",
+    initialState: initialState,
     reducers: {
-        increment: (state) => {
-            state.counter += 1;
+        add: (state, action) => {
+            state.todos.push(action.payload)
         },
-        decrement: state => {
-            state.counter -= 1;
+        remove: (state, action) => {
+            state.todos.splice(action.payload, 1)
         }
     }
 })
 
-// ACTION
-const { increment, decrement } = counterSlice.actions
+const { add, remove } = todoSlice.actions;
 
-// REDUCER
-const counterReducer = counterSlice.reducer
+const todoReducer = todoSlice.reducer;
 
-// STORE
 const store = configureStore({
     reducer: {
-        ctr: counterReducer
+        myReducer: todoReducer
     }
 })
 
-console.log("INITIAL STATE : ", store.getState())
 
-store.dispatch(increment())
+store.dispatch(add("renew car insurance"))
 
-console.log("AFTER INCREMENT : ", store.getState())
+console.log("AFTER ADD ", store.getState())
 
-store.dispatch(decrement());
+store.dispatch(remove(0))
 
-console.log("AFTER DECREMENT : ", store.getState())
+console.log("AFTER REMOVE ", store.getState())
+
+// store = { todos : [] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const initialState = {
+//     counter: 0
+// }
+// const initialResultState = {
+//     result: []
+// }
+// const counterSlice = createSlice({
+//     name: "counter",
+//     initialState,
+//     reducers: {
+//         increment: (state) => {
+//             state.counter += 1;
+//         },
+//         decrement: state => {
+//             state.counter -= 1;
+//         }
+//     }
+// })
+
+// const resultSlice = createSlice({
+//     name: "result",
+//     initialState: initialResultState,
+//     reducers: {
+//         save: (state, action) => {
+//             console.log("ACTION : ", action)
+//             state.result.push(action.payload)
+//         }
+//     }
+// })
+
+// // ACTION
+// const { increment, decrement } = counterSlice.actions
+// const { save } = resultSlice.actions;
+
+// // REDUCER
+// const counterReducer = counterSlice.reducer
+// const resultReducer = resultSlice.reducer
+
+// // STORE
+// const store = configureStore({
+//     reducer: {
+//         ctr: counterReducer,
+//         res: resultReducer
+//     }
+// })
+
+// console.log("INITIAL STATE : ", store.getState())
+
+// store.dispatch(increment())
+
+// console.log("AFTER INCREMENT : ", store.getState())
+
+// store.dispatch(decrement());
+
+// console.log("AFTER DECREMENT : ", store.getState())
+
+// let state = store.getState()
+
+// store.dispatch(save(state.ctr.counter))
+// console.log("AFTER SAVE : ", store.getState())
+
+// store.dispatch(increment())
+// console.log("AFTER INCREMENT : ", store.getState())
+
+// state = store.getState()
+
+// store.dispatch(save(state.ctr.counter))
+// console.log("AFTER SAVE : ", store.getState())
