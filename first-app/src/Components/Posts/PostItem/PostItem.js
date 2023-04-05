0e52ff486960b4ba55bff893bef3ef4d252921fe
Sumit Khandelwal <synergy2411@outlook.com>
2023-04-05 14:45:18 +0530

    dialog added

--- first-app/src/Components/Posts/PostItem/PostItem.js
+++ first-app/src/Components/Posts/PostItem/PostItem.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import "./PostItem.css";
 
 const PostItem = () => {
+    const { postId } = useParams();
+    const [post, setPost] = useState()
+
+    useEffect(() => {
+        axios.get(`http://localhost:3030/posts/${postId}`)
+            .then(response => setPost(response.data))
+            .catch(console.error)
+    }, [postId])
+
+    if (!post) {
+        return <p>Loading...</p>
+    }
     return (
-        <div>
-            post item coming soon...
+        <div className='backdrop'>
+            <div className='msg-dialog'>
+                <div className='card'>
+                    <div className='card-header'>
+                        <h6 className='text-center'>{post.title.toUpperCase()}</h6>
+                    </div>
+                    <div className='card-body'>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
+
 }
 
 export default PostItem;
--- first-app/src/Components/Posts/Posts.js
+++ first-app/src/Components/Posts/Posts.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Outlet, Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import "./Posts.css";
-import PostItem from './PostItem/PostItem';
 
 const Posts = () => {
     const [posts, setPosts] = useState([])
@@ -16,15 +15,15 @@ const Posts = () => {
     }, [])
 
     const navigate = useNavigate()
-    const postSelectHandler = () => {
-        navigate("/posts/p001")
+    const postSelectHandler = (postId) => {
+        navigate(`/posts/${postId}`)
     }
     return (
         <div>
             <h3 className='text-center'>My Posts</h3>
             <div className='row'>
                 {posts.map(post => <div className='col-4' key={post.id}>
-                    <div className='card sel-post' onClick={postSelectHandler}>
+                    <div className='card sel-post' onClick={() => postSelectHandler(post.id)}>
                         <div className='card-header'>
                             <h6 className='text-center'>{post.title.toUpperCase()}</h6>
                         </div>
