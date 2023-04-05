c5f557fa0ce85074827fbcc7e193638a7265a20f
Sumit Khandelwal <synergy2411@outlook.com>
2023-04-05 16:36:11 +0530

    lazy loading

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,30 +1,37 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Auth from './Components/Auth/Auth';
-import Expenses from './Components/Expenses/Expenses'
-import Todos from './Components/Todos/Todos'
+// import Auth from './Components/Auth/Auth';
+// import Expenses from './Components/Expenses/Expenses'
+// import Todos from './Components/Todos/Todos'
 import Header from './Components/Header/Header';
-import Posts from './Components/Posts/Posts';
-import PostItem from './Components/Posts/PostItem/PostItem';
+// import Posts from './Components/Posts/Posts';
+// import PostItem from './Components/Posts/PostItem/PostItem';
+import React, { Suspense } from 'react';
+
+const Auth = React.lazy(() => import("./Components/Auth/Auth"))
+const Expenses = React.lazy(() => import('./Components/Expenses/Expenses'))
+const Todos = React.lazy(() => import('./Components/Todos/Todos'))
+const Posts = React.lazy(() => import('./Components/Posts/Posts'))
+const PostItem = React.lazy(() => import('./Components/Posts/PostItem/PostItem'))
 
 function App() {
   return (
-    <div className="container">
-      <Header />
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="container">
+        <Header />
 
-      <p className='text-center display-4'>My First App</p>
+        <Routes>
+          <Route path='/' element={<Navigate to="/auth" />} />
+          <Route path={"/auth"} element={<Auth />} />
+          <Route path={"/todos"} element={<Todos />} />
+          <Route path={"/expenses"} element={<Expenses />} />
+          <Route path={"/posts/*"} element={<Posts />}>
+            <Route path=':postId' element={<PostItem />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
 
-      <Routes>
-        <Route path='/' element={<Navigate to="/auth" />} />
-        <Route path={"/auth"} element={<Auth />} />
-        <Route path={"/todos"} element={<Todos />} />
-        <Route path={"/expenses"} element={<Expenses />} />
-        <Route path={"/posts/*"} element={<Posts />}>
-          <Route path=':postId' element={<PostItem />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-
-    </div>
+      </div>
+    </Suspense>
   );
 }
 
